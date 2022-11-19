e0b3b2b200e541ab273ad5b0f350e44b638d3b26
Dalia <dalya1812@gmail.com>
2022-11-19 18:32:54 +0200

    update home page, creat welcome page

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import $ from 'jquery'
-import { createUser } from './rest';
-import { openConnection, sendPlainMessage } from './sockets';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from "jquery";
+import { createUser } from "./rest";
+import { openConnection, sendPlainMessage } from "./sockets";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 $(() => {
-  $(document).on('submit', () => {
+  $(document).on("submit", () => {
     const user = {
-      email: $('#emailInput').val(),
-      nickName: $('#userInput').val(),
-      password: $('#passwordInput').val()
-    }
+      email: $("#emailInput").val(),
+      nickName: $("#userInput").val(),
+      password: $("#passwordInput").val(),
+    };
     createUser(user);
-  })
+  });
 
   $("#send-btn").on("click", () => {
-    sendPlainMessage("MyUser", $('#message-input').val())
-  })
-
-})
+    sendPlainMessage("MyUser", $("#message-input").val());
+  });
+});
 openConnection();
