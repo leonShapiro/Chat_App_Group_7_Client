f8be1fa231f72a2b624db068aa3b2629ecedeecd
Dalia <dalya1812@gmail.com>
2022-11-20 10:12:32 +0200

    orginaze pages

---
+++ dist/Entities/user.js
@@ -0,0 +1,7 @@
+class User {
+  constructor(nickName, email, password) {
+    this.nickName = nickName;
+    this.email = email;
+    this.password = password;
+  }
+}
--- dist/registration.js
+++ dist/registration.js
@@ -1,12 +1,12 @@
-users = [];
-function creatUser(event) {
-  event.preventDefault();
-  const data = new FormData(event.target);
-  const user = Object.fromEntries(data.entries());
-  const json = JSON.stringify(user);
-  console.log({ json });
-  users.push(json);
-  localStorage.setItem("form", users);
-}
-const form = document.querySelector("form");
-form.addEventListener("submit", creatUser);
+// users = [];
+// function creatUser(event) {
+//   event.preventDefault();
+//   const data = new FormData(event.target);
+//   const user = Object.fromEntries(data.entries());
+//   const json = JSON.stringify(user);
+//   console.log({ json });
+//   users.push(json);
+//   localStorage.setItem("form", users);
+// }
+// const form = document.querySelector("form");
+// form.addEventListener("submit", creatUser);
