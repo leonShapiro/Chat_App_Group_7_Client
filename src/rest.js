434b8d19c957966ca10e855f365d9cbfd232f57b
Leon12215 <leon12215@gmail.com>
2022-11-22 15:40:08 +0200

    Guests now have tokens and can enter the main chat

--- src/rest.js
+++ src/rest.js
@@ -15,7 +15,8 @@ const createUser = (user) => {
 };
 
 const createGuest = (user) => {
-  fetch(serverAddress + "/user/guest", {
+  const joinAsGuestPromise = 
+  fetch(serverAddress + "/auth/guest", {
     method: "POST",
     body: JSON.stringify({
       nickName: user.nickName,
@@ -24,6 +25,18 @@ const createGuest = (user) => {
       "Content-Type": "application/json",
     },
   });
+
+  joinAsGuestPromise.then((Response) => {
+    if(Response.ok){
+      Response.text().then((text) => {
+
+        const myArray = text.split(":");
+        sessionStorage.setItem("nickName",myArray[0])
+        sessionStorage.setItem("token",myArray[1])
+        window.location.replace("./pages/chat.html");
+      })
+    }
+  })
 };
 
 const createMessage = (token,message) => {
