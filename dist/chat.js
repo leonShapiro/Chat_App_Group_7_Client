f9c00ca9a0f165f3c4355f826c75d2081f253e28
Leon12215 <leon12215@gmail.com>
2022-11-24 12:31:49 +0200

    asdasd

--- dist/chat.js
+++ dist/chat.js
@@ -1,24 +1,35 @@
 import { getAllUsers } from "../src/rest";
 
-displayUsers();
+$(() => {
+  window.alert("dasdasd")
 
-async function displayUsers() {
-  try {
-    const users = await getAllUsers();
-    for (var key in users) {
-      addUserToList(users[key]);
-    }
-  } catch (e) {
-    console.log(e);
+  if(sessionStorage.getItem("token") == null){
+    window.location.replace("http://localhost:9000/");
   }
-}
+  
+  displayUsers();
+  
+  async function displayUsers() {
+    try {
+      const users = await getAllUsers();
+      for (var key in users) {
+        addUserToList(users[key]);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  
+  function addUserToList(user) {
+    const list = document.querySelector("#user-list");
+    const row = document.createElement("tr");
+    row.innerHTML = `
+              <td><a href=”“>${user.nickName}</a></td>
+              <td><i class="bi bi-person"></i></td>
+          `;
+    list.appendChild(row);
+  }
+  
 
-function addUserToList(user) {
-  const list = document.querySelector("#user-list");
-  const row = document.createElement("tr");
-  row.innerHTML = `
-            <td><a href=”“>${user.nickName}</a></td>
-            <td><i class="bi bi-person"></i></td>
-        `;
-  list.appendChild(row);
-}
+})
+
